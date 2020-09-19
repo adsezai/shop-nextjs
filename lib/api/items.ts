b1b9711f021239af9b91833a4f46bd66c23b5222
adosez <sezairi.a@googlemail.com>
2020-09-19 16:14:24 +0200

    added connection of homepage to specific itempage

--- lib/api/items.ts
+++ lib/api/items.ts
@@ -1,5 +1,26 @@
 import axios from './axios'
 
+export async function getItemList(page: number, limit: number, filter: string, coordinates: any, radius: any) {
+  const res = await axios.post(
+    'items/paginated',
+    {
+      searchOptions: {
+        page,
+        limit,
+        filter,
+        coordinates,
+        radius
+      }
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+  return res.data
+}
+
 export async function getItem(id: string | string[]) {
   const res = await axios.get(`items/item/${id}`)
   return res.data
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,43 @@
+import { GetServerSideProps } from 'next'
+import Link from 'next/link'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getItemList } from '../lib/api/items'
+import { Item } from '../lib/common/item.interface'
 
-export default function Home() {
+interface Props {
+  items: Array<Item>
+}
+export default function Home({ items }: Props) {
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Shop</p>
-        <p>Adis Shop Nextjs Project</p>
+        <p>Home</p>
       </section>
+      {items.map(item => (
+        <div>
+          <Link href={`/i/${item._id}`}>
+            <a>{item.title}</a>
+          </Link>
+        </div>
+      ))}
     </Layout>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  let items: null | Array<Item> = null
+
+  try {
+    items = await getItemList(0, 20, null, null, null)
+  } catch (error) {}
+  return {
+    props: {
+      items
+    }
+  }
 }
