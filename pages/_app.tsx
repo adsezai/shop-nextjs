d4f51cd15b1e0c756dc7d357098d7f8be05011d0
Adis S <sezairi.a@gmail.com>
2021-06-01 21:40:10 +0200

    added reset css and modified global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import '../styles/global.css'
-
 import { AppProps } from 'next/app'
 import Error from 'next/error'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import { darkTheme, theme } from '../styles/theme'
+
+import '../styles/reset.css'
+import '../styles/global.css'
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../mocks/index')
@@ -21,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1.0'></meta>
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
-          href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;500;700&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;600;700&display=swap'
           rel='stylesheet'
         />
       </Head>
