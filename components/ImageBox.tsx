65a3ab55ac442ce64d95db1573c21ceefeff6dd1
Adis S <sezairi.a@gmail.com>
2021-06-28 19:01:58 +0200

    add function to load mock image when in dev environment

--- components/ImageBox.tsx
+++ components/ImageBox.tsx
@@ -1,16 +1,13 @@
 import Image from 'next/image'
 import styled from 'styled-components'
 import { Box, Space, SpaceSize } from '../styles/utils'
+import { createImageURL } from '../lib/api/utils'
 
 export default function ImageBox({ images }: { images: Array<string> }) {
   return (
     <Box flexDirection='column'>
       <ImageContainer>
-        <Image
-          layout='fill'
-          objectFit='contain'
-          src={`https://adsezaistorage.blob.core.windows.net/adsezai/${images[0]}`}
-        ></Image>
+        <Image layout='fill' objectFit='contain' src={createImageURL(images[0])}></Image>
       </ImageContainer>
       <Space y={SpaceSize.medium} x={SpaceSize.medium}></Space>
       <Box>
@@ -18,12 +15,7 @@ export default function ImageBox({ images }: { images: Array<string> }) {
           (i, index) =>
             index > 0 && (
               <Thumbnail key={i} mr='10px'>
-                <Image
-                  src={`https://adsezaistorage.blob.core.windows.net/adsezai/${i}`}
-                  height='70px'
-                  width='70px'
-                  objectFit='cover'
-                ></Image>
+                <Image src={createImageURL(i)} height='70px' width='70px' objectFit='cover'></Image>
               </Thumbnail>
             )
         )}
